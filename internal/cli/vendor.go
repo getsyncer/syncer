7f39477bd8a54d3089d8af907867c3a088af9203
Jack Lindamood <jack@cresta.ai>
2023-07-24 09:42:15 -0700

    feat: resync

--- internal/cli/vendor.go
+++ internal/cli/vendor.go
@@ -2,13 +2,12 @@ package cli
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
-
 	"github.com/cresta/zapctx"
 	"github.com/getsyncer/syncer/internal/git"
 	"github.com/getsyncer/syncer/sharedapi/syncer"
 	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
 )
 
 type vendorCmd struct {
